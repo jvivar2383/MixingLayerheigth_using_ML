791c9530260f0adb429e80ae8d0356dd23a40c75
Jenifer Vivar <jvivar000@citymail.cuny.edu>
2023-07-02 18:17:48 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will use NYC LiDAR data to predict the height of the mixing layer height as well as the residual layer.',
+    author='Jenifer Vivar',
+    license='MIT',
+)
